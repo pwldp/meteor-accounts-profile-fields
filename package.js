50da603f79e810ba1adc1a0915cf4373876dea8e
pwldp <pawlakdp@gmail.com>
2014-07-18 12:05:06 +0200

    Add required 'sha' package

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use(['accounts-base'], ['client', 'server']);
+    api.use(['accounts-base','sha'], ['client', 'server']);
     api.add_files('accounts-profile-fields-server.js', 'server');
     api.add_files('accounts-profile-fields-client.js', 'client');
 });
