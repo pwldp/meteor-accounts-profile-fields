45395be8871c2d5830635f6aa0ffe2b09fd20f4e
madateam nagrody <d.pawlak@wurth.pl>
2014-07-18 11:48:58 +0200

    Remove srp from package.js

--- package.js
+++ package.js
@@ -1,10 +1,9 @@
 Package.describe({
-  summary: "Profile fields based log in, extended version of loginWithPassword() function"
+    summary: "Profile fields based log in, extended version of loginWithPassword() function"
 });
 
 Package.on_use(function (api) {
-  api.use(['accounts-base' , 'accounts-password'], ['client', 'server']);
-  api.use('srp', ['client', 'server']);
-  api.add_files('accounts-profile-fields-server.js', 'server');
-  api.add_files('accounts-profile-fields-client.js', 'client');
+    api.use(['accounts-base'], ['client', 'server']);
+    api.add_files('accounts-profile-fields-server.js', 'server');
+    api.add_files('accounts-profile-fields-client.js', 'client');
 });
