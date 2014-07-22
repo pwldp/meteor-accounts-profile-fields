34ef05f7340c68b866a8b84423d82789bac68c6b
pwldp <pawlakdp@gmail.com>
2014-07-22 13:10:24 +0200

    Update package.js

--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use(['accounts-base','sha'], ['client', 'server']);
+    api.use('check');
     api.add_files('accounts-profile-fields-server.js', 'server');
     api.add_files('accounts-profile-fields-client.js', 'client');
 });
